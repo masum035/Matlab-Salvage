683df6b9090106e5dfe27bf90f8976a16561ba60
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2021-04-27 10:06:33 +0600

    Date - 13 october,2020

---
+++ Simpson_Rule/Simpson_Rule_for_e_power-x.m
@@ -0,0 +1,30 @@
+lo = 0;
+hi = 1;
+
+f = @(x) exp(-x);
+
+h = 0.1;
+
+n = (hi - lo)/h;
+
+%cur = 0
+
+for i = 1 : n + 1
+    y(i) = f (lo + (i-1) * h);
+%    y(i) = f(cur);
+%    cur = cur + h;
+end
+
+I = 0;
+
+for i = 2 : 2 : n 
+    I = I + 4 * y(i);
+end
+
+for i = 3 : 2 : n - 1
+    I = I + 2 * y(i);
+end
+
+I = (h / 3) * ( y(1) + I + y(n+1) );
+
+disp(I);
