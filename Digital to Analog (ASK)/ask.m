604749d5fc53cc53ac72d0cfe55a1c9e0512963b
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2021-06-18 23:01:06 +0600

    Date - 11 June,2021

---
+++ Digital to Analog (ASK)/ask.m
@@ -0,0 +1,62 @@
+% Date 11 Jube,2021 
+% Abdullah Al Masum  
+clc;
+clear;
+close all;
+
+x = [1 0 1 1 0 1 0 1]; % Data
+% disp (x);
+
+bp = 0.000001; %bit period (if bit period is less than bit rate will be high)
+
+%% -------------------Digital Data--------------------%%
+bit = [];
+
+for n = 1:1:length(x)
+    if x(n) == 0    % if binary data is zero
+        se = zeros(1,100); 
+    else            % if binary data is one
+        se = ones(1,100);
+    end
+    bit = [bit se]; % here marging 2 arrays 
+end
+
+t1 = bp/100:bp/100:length(x)*bp; % time span is crucial
+
+subplot(2,1,1);
+plot(t1,bit, 'LineWidth', 2.5,color='#000'); 
+grid on;  % to clearly visualize line
+hline = refline(0); % baseline y-axis=0 heighlighting 
+hline.Color = 'r';
+axis([0 length(x)*bp -0.5 1.5]); % this can be done also by xlim() and ylim() 
+xlabel('Time');
+ylabel('Amplitude');
+title(["Digital Signal" num2str(x)]);
+
+%% -------------------Analog Signal(ASK)--------------------%%
+t2 = bp/100:bp/100:bp;   % time span is crucial
+
+A0 = 0;
+A1 = 1;
+
+f = 5/bp; % this,f means carriar frequency(high frequency) let's say multiple of 5 i'm considering
+
+sig = [];
+
+for n=1:1:length(x)
+    if x(n) == 0    % if binary data is zero
+        y = A0*sin(2*pi*f*t2);
+    else            % if binary data is one
+        y = A1*sin(2*pi*f*t2);  
+    end
+    sig = [sig y];
+end
+
+
+subplot(2,1,2);
+plot(t1,sig, 'LineWidth', 2.5,color='b'); 
+grid on;                         % to clearly visualize line
+axis([0 length(x)*bp -0.5 1.5]); % this can be done also by xlim() and ylim() 
+xlabel('Time');
+ylabel('Amplitude');
+title(["Analog Signal(ASK)" num2str(x)]);
